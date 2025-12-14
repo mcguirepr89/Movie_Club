3c451d70c3a9aa5efc6cf3f276daf0a99fed6627
Patrick McGuire <mcguirepr89@gmail.com>
2025-12-14 17:03:39 -0500

    added sorting!

--- tracker/views.py
+++ tracker/views.py
@@ -3,16 +3,50 @@ from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.db.models import Prefetch
+from django.contrib.auth.models import User
 from .forms import MovieForm, ViewingForm
-from .models import Movie, Viewing, Category
+from .models import Movie, Viewing, Category, StreamingService
 
 def movie_list(request):
     movies = Movie.objects.prefetch_related(
         "categories",
         "streaming_services",
-    )
+    ).all()
 
+    # --- Sorting ---
+    sort_by = request.GET.get("sort", "title")
+    sort_dir = request.GET.get("dir", "asc")
+    sort_fields = {"title": "title", "year": "year"}
+
+    if sort_by in sort_fields:
+        ordering = sort_fields[sort_by]
+        if sort_dir == "desc":
+            ordering = "-" + ordering
+        movies = movies.order_by(ordering)
+
+    # --- Filters ---
+    if request.user.is_authenticated:
+        # Seen/Unseen filter
+        seen_filter = request.GET.get("seen")
+        if seen_filter == "1":
+            movies = movies.filter(viewing__user=request.user)
+        elif seen_filter == "0":
+            movies = movies.exclude(viewing__user=request.user)
+
+    category_id = request.GET.get("category")
+    if category_id:
+        movies = movies.filter(categories__id=category_id)
+
+    recommender_id = request.GET.get("recommended_by")
+    if recommender_id:
+        movies = movies.filter(recommended_by__id=recommender_id)
+
+    streaming_id = request.GET.get("streaming")
+    if streaming_id:
+        movies = movies.filter(streaming_services__id=streaming_id)
+
+    # --- Viewings ---
     current_user_viewings = {}
     viewing_map = {}
 
@@ -24,9 +58,13 @@ def movie_list(request):
             else:
                 viewing_map.setdefault(v.movie_id, []).append(v)
     else:
-        # Anonymous users: show all viewings as "others"
         for v in Viewing.objects.select_related("user", "movie"):
             viewing_map.setdefault(v.movie_id, []).append(v)
+
+    # --- Filter data for drop-downs ---
+    categories = Category.objects.all()
+    streaming_services = StreamingService.objects.all()
+    recommenders = User.objects.filter(recommended_movies__isnull=False).distinct()
 
     return render(
         request,
@@ -35,6 +73,15 @@ def movie_list(request):
             "movies": movies,
             "current_user_viewings": current_user_viewings,
             "viewing_map": viewing_map,
+            "categories": categories,
+            "streaming_services": streaming_services,
+            "recommenders": recommenders,
+            "sort_by": sort_by,
+            "sort_dir": sort_dir,
+            "selected_seen": request.GET.get("seen", ""),
+            "selected_category": request.GET.get("category", ""),
+            "selected_recommender": request.GET.get("recommended_by", ""),
+            "selected_streaming": request.GET.get("streaming", ""),
         },
     )
 
